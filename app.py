ac70b0e09b33bfb7c28b658ff8f028df9d0b2266
excelledia <76048560+excelledia@users.noreply.github.com>
2021-07-06 18:37:59 +0200

    Update app.py
    Remove extra code (not used)

--- app.py
+++ app.py
@@ -8,26 +8,6 @@ from flask_cors import CORS
 app = Flask(__name__,template_folder='template')
 CORS(app)
 
-@app.route('/api/v1.0/response', methods=['POST'])
-def chatbot_response_json():
-    print(request)
-    if not request.json or not 'query' in request.json:
-        abort(400)
-    user = 1
-    response = str(get_response(request.json['query'],user))
-    print(response)
-    return jsonify({'message': response})
-
-@app.route('/api/v1.0/test', methods=['GET'])
-def chatbot_test():
-    # if not request.json or not 'query' in request.json:
-    #     abort(400)
-    # user = 1
-    # response = str(get_response(request.json['query'],user))
-    # print(response)
-    # return jsonify({'message': response})
-    return ('jjjj')
-
 @app.route("/")
 def home():
     return render_template("chatbot.html")
